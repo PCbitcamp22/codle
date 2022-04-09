6b7b0197bde5f931322d1488371fa474ca6ba4db
Arav Dave <aravdave@gmail.com>
2022-04-09 11:16:21 -0400

    Problem and Test components

---
+++ src/Problem.js
@@ -0,0 +1,10 @@
+import React from "React"
+
+export default function Problem() {
+
+  
+
+  return (
+    <h1>Q: {question}</h1>
+  )
+}
---
+++ src/Tests.js
@@ -0,0 +1,20 @@
+import React from "React"
+
+export default function Tests() {
+
+  const [testOne, setTestOne] = React.useState({
+    status: "notrun"
+  })
+
+  function TestBox() {
+    return ({
+      
+    })
+  }
+
+  return (
+    <div >
+      <TestBox color="white" />
+    </div>
+  )
+}
