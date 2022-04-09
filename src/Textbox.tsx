f3a6d867b23c725b6e229a19e0a186e189705130
g-kienzle <kienzle2003@gmail.com>
2022-04-09 12:21:26 -0400

    Updated to React 18, added css

--- src/Textbox.tsx
+++ src/Textbox.tsx
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
 import './App.css';
+import './Textbox.css';
 
 function Textbox() {
 
@@ -11,37 +11,50 @@ function Textbox() {
         document.getElementById(respname).style.display = "block";
     }
 
+    function sendCode(respNum) {
+        var x = Array.from(document.getElementsByClassName("resp") as HTMLCollectionOf<HTMLElement>);
+        console.log(x[respNum].textContent); //Send this to API
+        x[respNum].textContent = "";
+        this.props.fn();
+    }
+
+    
 
     return (
         <div className="Textbox">
+            {"this.props.responseCount"}
         
-        <form className="form">
-            <div className="problembar">
-                <button onClick={() => openResponse('Response 1')}>Response 1</button>
-                <button onClick={() => openResponse('Response 2')}>Response 2</button>
-                <button onClick={() => openResponse('Response 3')}>Response 3</button>
-            </div>
-            <div id="resp1" className="resp" style={{display: "block"}}>
-                <textarea className="resp1"></textarea>
-            </div>
-            <div id="resp2" className="resp" style={{display: "none"}}>
-                <textarea className="resp2"></textarea>
-            </div>
-            <div id="resp3" className="resp" style={{display: "none"}}>
-                <textarea className="resp3"></textarea>
-            </div>
-            <div id="resp4" className="resp" style={{display: "none"}}>
-                <textarea className="resp4"></textarea>
-            </div>
-            <div id="resp5" className="resp" style={{display: "none"}}>
-                <textarea className="resp5"></textarea>
-            </div>
-            <div id="resp6" className="resp" style={{display: "none"}}>
-                <textarea className="resp6"></textarea>
-            </div>
-            
+            <form className="form">
+                {/* <div className="problembar">
+                    <button onClick={() => openResponse('Response 1')}>Response 1</button>
+                    <button onClick={() => openResponse('Response 2')}>Response 2</button>
+                    <button onClick={() => openResponse('Response 3')}>Response 3</button>
+                    <button onClick={() => openResponse('Response 4')}>Response 4</button>
+                    <button onClick={() => openResponse('Response 5')}>Response 5</button>
+                    <button onClick={() => openResponse('Response 6')}>Response 6</button>
+                </div> */}
+                <div id="resp1" className="resp" style={{display: "block"}}>
+                    <textarea className="resp1 box" rows={20} cols={80}></textarea>
+                </div>
+                {/* <div id="resp2" className="resp" style={{display: "none"}}>
+                    <textarea className="resp2 box"></textarea>
+                </div>
+                <div id="resp3" className="resp" style={{display: "none"}}>
+                    <textarea className="resp3 box"></textarea>
+                </div>
+                <div id="resp4" className="resp" style={{display: "none"}}>
+                    <textarea className="resp4 box"></textarea>
+                </div>
+                <div id="resp5" className="resp" style={{display: "none"}}>
+                    <textarea className="resp5 box"></textarea>
+                </div>
+                <div id="resp6" className="resp" style={{display: "none"}}>
+                    <textarea className="resp6 box"></textarea>
+                </div> */}
+                <button className='submit' onClick={() => sendCode(this.props.responseCount)}>Submit</button>
+                
 
-        </form>
+            </form>
         </div>
     );
 }
